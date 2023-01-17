afcd4ad950efd539efd9788b7b8d593d56ff6886
Djam18 <adamdjamal18@gmail.com>
2023-01-17 12:00:00 +0100

    feat(admin): gestion des paiements

---
+++ app/Http/Controllers/Backend/PaymentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    // Contrôleur pour PaymentController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/admin/payment/index.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\admin\payment;
+
+// Classe index.blade
+class index.blade
+{
+    // Implémentation à venir
+}
