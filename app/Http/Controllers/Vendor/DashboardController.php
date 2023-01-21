34c9903d8a7ad5281111e1846da80c90feaf9702
Djam18 <adamdjamal18@gmail.com>
2023-01-21 12:00:00 +0100

    feat(vendor): tableau de bord vendeur

---
+++ app/Http/Controllers/Vendor/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    // Contrôleur pour DashboardController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/vendor/dashboard.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\vendor;
+
+// Classe dashboard.blade
+class dashboard.blade
+{
+    // Implémentation à venir
+}
