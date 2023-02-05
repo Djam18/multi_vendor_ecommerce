98b352f6c2af8d7813e40c903dff0467c06aacb3
Djam18 <adamdjamal18@gmail.com>
2023-02-05 12:00:00 +0100

    feat: implémentation des galeries d'images pour produits

---
+++ app/Http/Controllers/Vendor/ProductGalleryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductGalleryController extends Controller
+{
+    // Contrôleur pour ProductGalleryController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
