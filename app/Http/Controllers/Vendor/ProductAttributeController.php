124d8915e80324d9c1f1dbf7412e6351c188a020
Djam18 <adamdjamal18@gmail.com>
2023-02-08 12:00:00 +0100

    feat: système de gestion des attributs produits

---
+++ app/Http/Controllers/Vendor/ProductAttributeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductAttributeController extends Controller
+{
+    // Contrôleur pour ProductAttributeController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
