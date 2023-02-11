55359b4413c365f3a9b0a3fcabb3c4db28e1e209
Djam18 <adamdjamal18@gmail.com>
2023-02-11 12:00:00 +0100

    feat: implémentation des variantes de produits

---
+++ app/Http/Controllers/Vendor/ProductVariantController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductVariantController extends Controller
+{
+    // Contrôleur pour ProductVariantController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
