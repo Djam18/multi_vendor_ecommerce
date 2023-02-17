3b1af18cb6e0625b68701a89b973c70381039abe
Djam18 <adamdjamal18@gmail.com>
2023-02-17 12:00:00 +0100

    feat(frontend): catalogue de produits

---
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    // Contrôleur pour ProductController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/product/products.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\product;
+
+// Classe products.blade
+class products.blade
+{
+    // Implémentation à venir
+}
