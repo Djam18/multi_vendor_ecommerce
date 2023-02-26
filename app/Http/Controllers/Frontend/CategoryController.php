f48526bc93e7ad6c542471d8d9975abc916167c9
Djam18 <adamdjamal18@gmail.com>
2023-02-26 12:00:00 +0100

    feat(frontend): filtrage des produits par catégorie

---
+++ app/Http/Controllers/Frontend/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    // Contrôleur pour CategoryController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/category.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend;
+
+// Classe category.blade
+class category.blade
+{
+    // Implémentation à venir
+}
