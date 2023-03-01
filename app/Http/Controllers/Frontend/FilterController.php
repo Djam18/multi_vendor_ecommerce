1d10ea4ede08bdc6833ff4179895586108e43db4
Djam18 <adamdjamal18@gmail.com>
2023-03-01 12:00:00 +0100

    feat(frontend): filtrage des produits par prix

---
+++ app/Http/Controllers/Frontend/FilterController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FilterController extends Controller
+{
+    // Contrôleur pour FilterController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/components/price_filter.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\components;
+
+// Classe price_filter.blade
+class price_filter.blade
+{
+    // Implémentation à venir
+}
