2dffb3d2959abb71851d0c012027cfa80c8e6d6a
Djam18 <adamdjamal18@gmail.com>
2023-03-22 12:00:00 +0100

    feat(payment): intégration de Stripe

---
+++ app/Http/Controllers/Payment/StripePaymentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Payment;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StripePaymentController extends Controller
+{
+    // Contrôleur pour StripePaymentController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/payment/stripe.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\payment;
+
+// Classe stripe.blade
+class stripe.blade
+{
+    // Implémentation à venir
+}
