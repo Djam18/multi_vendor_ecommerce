64ea0d89b36bbebf48245acf22553788c577343a
Djam18 <adamdjamal18@gmail.com>
2023-03-25 12:00:00 +0100

    feat(payment): intégration de PayPal

---
+++ app/Http/Controllers/Payment/PaypalPaymentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Payment;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaypalPaymentController extends Controller
+{
+    // Contrôleur pour PaypalPaymentController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/payment/paypal.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\payment;
+
+// Classe paypal.blade
+class paypal.blade
+{
+    // Implémentation à venir
+}
