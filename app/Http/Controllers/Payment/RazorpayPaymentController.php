b87b64a175643fe9c3bbba45cb24a482f76c4872
Djam18 <adamdjamal18@gmail.com>
2023-03-28 12:00:00 +0100

    feat(payment): intégration de Razorpay

---
+++ app/Http/Controllers/Payment/RazorpayPaymentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Payment;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RazorpayPaymentController extends Controller
+{
+    // Contrôleur pour RazorpayPaymentController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/payment/razorpay.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\payment;
+
+// Classe razorpay.blade
+class razorpay.blade
+{
+    // Implémentation à venir
+}
