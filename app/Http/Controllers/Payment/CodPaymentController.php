ab408559f7d089b588d76f119af0ca22a4f41eb5
Djam18 <adamdjamal18@gmail.com>
2023-03-31 12:00:00 +0100

    feat(payment): paiement à la livraison (COD)

---
+++ app/Http/Controllers/Payment/CodPaymentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Payment;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CodPaymentController extends Controller
+{
+    // Contrôleur pour CodPaymentController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/payment/cod.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\payment;
+
+// Classe cod.blade
+class cod.blade
+{
+    // Implémentation à venir
+}
