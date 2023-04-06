a6b1b58b29341b29212905fe6700f17dae78f805
Djam18 <adamdjamal18@gmail.com>
2023-04-06 12:00:00 +0100

    feat(order): suivi des commandes

---
+++ app/Http/Controllers/Frontend/OrderTrackingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrderTrackingController extends Controller
+{
+    // Contrôleur pour OrderTrackingController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/order_tracking.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend;
+
+// Classe order_tracking.blade
+class order_tracking.blade
+{
+    // Implémentation à venir
+}
