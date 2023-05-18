f797bb385f6770ad5e6646ae4ef567228cf07cc4
Djam18 <adamdjamal18@gmail.com>
2023-05-18 12:00:00 +0100

    feat(vendor): gestion du profil boutique

---
+++ app/Http/Controllers/Vendor/ShopProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShopProfileController extends Controller
+{
+    // Contrôleur pour ShopProfileController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/vendor/shop/profile.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\vendor\shop;
+
+// Classe profile.blade
+class profile.blade
+{
+    // Implémentation à venir
+}
