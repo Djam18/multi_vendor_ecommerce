4bc430fbd3aecb78de86713a400e89c8396ed03f
Djam18 <adamdjamal18@gmail.com>
2023-05-21 12:00:00 +0100

    feat(vendor): système de commissions

---
+++ app/Http/Controllers/Backend/CommissionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CommissionController extends Controller
+{
+    // Contrôleur pour CommissionController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
