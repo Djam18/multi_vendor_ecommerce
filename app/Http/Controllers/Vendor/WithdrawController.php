6f69723661065c44420f394e7a2f619c12382f2d
Djam18 <adamdjamal18@gmail.com>
2023-05-27 12:00:00 +0100

    feat(vendor): demandes de retrait

---
+++ app/Http/Controllers/Vendor/WithdrawController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WithdrawController extends Controller
+{
+    // Contrôleur pour WithdrawController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
