69abcef196bfc304d9f62d3e86e1509a248f7469
Djam18 <adamdjamal18@gmail.com>
2023-06-20 12:00:00 +0100

    feat(vendor): gestion des stocks

---
+++ app/Http/Controllers/Vendor/InventoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InventoryController extends Controller
+{
+    // Contrôleur pour InventoryController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/vendor/inventory/index.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\vendor\inventory;
+
+// Classe index.blade
+class index.blade
+{
+    // Implémentation à venir
+}
