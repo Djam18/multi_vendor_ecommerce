fc20cde7baa29d1d562b24cda89535e521186462
Djam18 <adamdjamal18@gmail.com>
2023-06-26 12:00:00 +0100

    feat(vendor): personnalisation de la boutique

---
+++ app/Http/Controllers/Vendor/ShopSettingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShopSettingController extends Controller
+{
+    // Contrôleur pour ShopSettingController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/vendor/shop/settings.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\vendor\shop;
+
+// Classe settings.blade
+class settings.blade
+{
+    // Implémentation à venir
+}
