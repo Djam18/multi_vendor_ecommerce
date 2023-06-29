b204e3a6ca56e0fbf43754654003859a99148c47
Djam18 <adamdjamal18@gmail.com>
2023-06-29 12:00:00 +0100

    feat(vendor): gestion des avis produits

---
+++ app/Http/Controllers/Vendor/ReviewController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    // Contrôleur pour ReviewController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
