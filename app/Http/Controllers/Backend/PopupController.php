6f72f94c35e361beb691223884d3274126f6f0a0
Djam18 <adamdjamal18@gmail.com>
2023-08-19 12:00:00 +0100

    feat(admin): gestion des popups

---
+++ app/Http/Controllers/Backend/PopupController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PopupController extends Controller
+{
+    // Contrôleur pour PopupController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
