e8fdd9f31d3a3f20c5aa8ace93d0a34b1adf1e98
Djam18 <adamdjamal18@gmail.com>
2023-08-25 12:00:00 +0100

    feat(admin): gestion des rôles et permissions

---
+++ app/Http/Controllers/Backend/RoleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    // Contrôleur pour RoleController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
