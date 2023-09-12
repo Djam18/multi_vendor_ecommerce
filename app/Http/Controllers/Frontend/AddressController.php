e0ab1c6a5afb1415cb20c29704200a20b603a25e
Djam18 <adamdjamal18@gmail.com>
2023-09-12 12:00:00 +0100

    feat(user): gestion des adresses

---
+++ app/Http/Controllers/Frontend/AddressController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    // Contrôleur pour AddressController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
