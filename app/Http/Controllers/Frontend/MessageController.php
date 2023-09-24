5e517ab39f6338966ee349ca20b519859413e3d3
Djam18 <adamdjamal18@gmail.com>
2023-09-24 12:00:00 +0100

    feat(user): système de messagerie

---
+++ app/Http/Controllers/Frontend/MessageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    // Contrôleur pour MessageController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
