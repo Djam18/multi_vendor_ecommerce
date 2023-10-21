031271b6b2ff5ff09c09649a6a2be068f98e1a03
Djam18 <adamdjamal18@gmail.com>
2023-10-21 12:00:00 +0100

    feat(frontend): carrousel de produits

---
+++ resources/js/carousel.js
@@ -0,0 +1,7 @@
+/**
+ * carousel JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour carousel
---
+++ resources/views/frontend/components/product_carousel.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\components;
+
+// Classe product_carousel.blade
+class product_carousel.blade
+{
+    // Implémentation à venir
+}
