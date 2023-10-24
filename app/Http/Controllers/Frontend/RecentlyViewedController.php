89663a1d3fd77eaab006214847ea75030821ba6a
Djam18 <adamdjamal18@gmail.com>
2023-10-24 12:00:00 +0100

    feat(frontend): produits récemment consultés

---
+++ app/Http/Controllers/Frontend/RecentlyViewedController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RecentlyViewedController extends Controller
+{
+    // Contrôleur pour RecentlyViewedController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/components/recently_viewed.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\components;
+
+// Classe recently_viewed.blade
+class recently_viewed.blade
+{
+    // Implémentation à venir
+}
