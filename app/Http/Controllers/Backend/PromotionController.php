45b7d44e47b5fd498daadc1689283160e70b1c7d
Djam18 <adamdjamal18@gmail.com>
2023-11-02 12:00:00 +0100

    feat(marketing): produits en promotion

---
+++ app/Http/Controllers/Backend/PromotionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PromotionController extends Controller
+{
+    // Contrôleur pour PromotionController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
