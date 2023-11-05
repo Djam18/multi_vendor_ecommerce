537fb89ff1bb81e4c5ccceb9929f23393d5cb33c
Djam18 <adamdjamal18@gmail.com>
2023-11-05 12:00:00 +0100

    feat(marketing): produits vedettes

---
+++ app/Http/Controllers/Backend/FeaturedProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FeaturedProductController extends Controller
+{
+    // Contrôleur pour FeaturedProductController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/admin/featured/index.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\admin\featured;
+
+// Classe index.blade
+class index.blade
+{
+    // Implémentation à venir
+}
