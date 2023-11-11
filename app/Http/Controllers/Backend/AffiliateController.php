96cb564cce1e2865989eecd7e3f5bc7acd00958c
Djam18 <adamdjamal18@gmail.com>
2023-11-11 12:00:00 +0100

    feat(marketing): système d'affiliation

---
+++ app/Http/Controllers/Backend/AffiliateController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AffiliateController extends Controller
+{
+    // Contrôleur pour AffiliateController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
