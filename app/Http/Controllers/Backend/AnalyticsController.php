fd6b2d75eb12e89c01424f6267c389376025329e
Djam18 <adamdjamal18@gmail.com>
2023-12-20 12:00:00 +0100

    feat(marketing): intégration Google Analytics

---
+++ app/Http/Controllers/Backend/AnalyticsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AnalyticsController extends Controller
+{
+    // Contrôleur pour AnalyticsController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/admin/analytics/settings.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\admin\analytics;
+
+// Classe settings.blade
+class settings.blade
+{
+    // Implémentation à venir
+}
