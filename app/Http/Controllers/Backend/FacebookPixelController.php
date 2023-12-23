ebd609c91ce11cf1878a7cbd91e14615166767c0
Djam18 <adamdjamal18@gmail.com>
2023-12-23 12:00:00 +0100

    feat(marketing): intégration Facebook Pixel

---
+++ app/Http/Controllers/Backend/FacebookPixelController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FacebookPixelController extends Controller
+{
+    // Contrôleur pour FacebookPixelController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/admin/pixel/settings.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\admin\pixel;
+
+// Classe settings.blade
+class settings.blade
+{
+    // Implémentation à venir
+}
