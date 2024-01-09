bc287f5ca79147256f792b43a2b37c862cae41cc
Djam18 <adamdjamal18@gmail.com>
2024-01-09 12:00:00 +0100

    feat(advanced): personnalisation du tableau de bord utilisateur

---
+++ app/Http/Controllers/Frontend/DashboardCustomizationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardCustomizationController extends Controller
+{
+    // Contrôleur pour DashboardCustomizationController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/dashboard/customize.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\dashboard;
+
+// Classe customize.blade
+class customize.blade
+{
+    // Implémentation à venir
+}
