5e55cc39d28341095f724fea6d92051ff0a7191a
Djam18 <adamdjamal18@gmail.com>
2024-01-11 12:00:00 +0100

    feat(social): login avec réseaux sociaux

---
+++ app/Http/Controllers/Auth/SocialLoginController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SocialLoginController extends Controller
+{
+    // Contrôleur pour SocialLoginController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
