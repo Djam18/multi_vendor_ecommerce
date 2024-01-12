307bceb36ebbe9634088869dffc37b9a399c5b60
Djam18 <adamdjamal18@gmail.com>
2024-01-12 12:00:00 +0100

    feat(advanced): système de suggestions intelligentes

---
+++ app/Http/Controllers/Frontend/SuggestionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SuggestionController extends Controller
+{
+    // Contrôleur pour SuggestionController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ app/Services/SmartSuggestionService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe SmartSuggestionService
+class SmartSuggestionService
+{
+    // Implémentation à venir
+}
