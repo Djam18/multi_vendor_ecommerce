2f92ee7c4a5b4ffccef4d790ba2869bcbc9557e4
Djam18 <adamdjamal18@gmail.com>
2024-01-17 12:00:00 +0100

    feat(social): forum de discussion

---
+++ app/Http/Controllers/Frontend/ForumController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ForumController extends Controller
+{
+    // Contrôleur pour ForumController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
