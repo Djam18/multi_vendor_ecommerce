190f33769713f36c76cf188e3455528e82ef8f75
Djam18 <adamdjamal18@gmail.com>
2024-01-20 12:00:00 +0100

    feat(social): blogs et articles

---
+++ resources/views/frontend/blog/detail.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\blog;
+
+// Classe detail.blade
+class detail.blade
+{
+    // Implémentation à venir
+}
---
+++ resources/views/frontend/blog/index.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\blog;
+
+// Classe index.blade
+class index.blade
+{
+    // Implémentation à venir
+}
