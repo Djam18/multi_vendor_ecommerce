87de18dde5452a4cef91f08a8b565bc786fdc289
Djam18 <adamdjamal18@gmail.com>
2024-01-27 12:00:00 +0100

    feat(advanced): personnalisation de l'interface utilisateur

---
+++ app/Services/UICustomizationService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe UICustomizationService
+class UICustomizationService
+{
+    // Implémentation à venir
+}
---
+++ resources/views/frontend/customize.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend;
+
+// Classe customize.blade
+class customize.blade
+{
+    // Implémentation à venir
+}
