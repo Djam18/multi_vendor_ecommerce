f776fb7a314be18d3c33eaddde1d95c9538f0241
Djam18 <adamdjamal18@gmail.com>
2024-01-30 12:00:00 +0100

    feat(advanced): système de rapports personnalisés

---
+++ app/Http/Controllers/Backend/CustomReportController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CustomReportController extends Controller
+{
+    // Contrôleur pour CustomReportController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ app/Services/CustomReportService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe CustomReportService
+class CustomReportService
+{
+    // Implémentation à venir
+}
