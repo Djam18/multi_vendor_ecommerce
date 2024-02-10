3a1aff60f0e0205237eca6db5c91891fa9e7ceb1
Djam18 <adamdjamal18@gmail.com>
2024-02-10 12:00:00 +0100

    feat(UX): lazy loading des images

---
+++ resources/js/lazy-loading.js
@@ -0,0 +1,7 @@
+/**
+ * lazy-loading JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour lazy-loading
---
+++ resources/views/components/lazy-image.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\components;
+
+// Classe lazy-image.blade
+class lazy-image.blade
+{
+    // Implémentation à venir
+}
