6d9ae2f8660c56561085e8b7c3814043c5182d31
Djam18 <adamdjamal18@gmail.com>
2024-02-13 12:00:00 +0100

    feat(UX): recherche instantanée

---
+++ app/Http/Controllers/Frontend/InstantSearchController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InstantSearchController extends Controller
+{
+    // Contrôleur pour InstantSearchController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/js/instant-search.js
@@ -0,0 +1,7 @@
+/**
+ * instant-search JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour instant-search
