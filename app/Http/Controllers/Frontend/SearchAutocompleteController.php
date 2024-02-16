ad6636cca74f3e492d0065fd80b5008e1e7bb952
Djam18 <adamdjamal18@gmail.com>
2024-02-16 12:00:00 +0100

    feat(UX): autocomplétion de recherche

---
+++ app/Http/Controllers/Frontend/SearchAutocompleteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SearchAutocompleteController extends Controller
+{
+    // Contrôleur pour SearchAutocompleteController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/js/search-autocomplete.js
@@ -0,0 +1,7 @@
+/**
+ * search-autocomplete JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour search-autocomplete
