1ddd29e0808857a3e8364bcfc56be58513b4beb3
Djam18 <adamdjamal18@gmail.com>
2024-03-08 12:00:00 +0100

    feat(integration): webhooks pour événements

---
+++ app/Http/Controllers/Api/WebhookController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WebhookController extends Controller
+{
+    // Contrôleur pour WebhookController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
