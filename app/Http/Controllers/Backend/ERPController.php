2bfb3f4a24e6c9266bf3ee2fec18201a27c54b7b
Djam18 <adamdjamal18@gmail.com>
2024-03-29 12:00:00 +0100

    feat(integration): intégration ERP

---
+++ app/Http/Controllers/Backend/ERPController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ERPController extends Controller
+{
+    // Contrôleur pour ERPController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ app/Services/ERPIntegrationService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe ERPIntegrationService
+class ERPIntegrationService
+{
+    // Implémentation à venir
+}
