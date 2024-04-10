e4a9c48737bae0048a9c6a33b20561e09d8040ad
Djam18 <adamdjamal18@gmail.com>
2024-04-10 12:00:00 +0100

    feat(integration): intégration services de livraison

---
+++ app/Http/Controllers/Backend/ShippingIntegrationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShippingIntegrationController extends Controller
+{
+    // Contrôleur pour ShippingIntegrationController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ app/Services/ShippingIntegrationService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe ShippingIntegrationService
+class ShippingIntegrationService
+{
+    // Implémentation à venir
+}
