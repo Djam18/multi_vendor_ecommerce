e2edf9fa4592e833d34d5004b8c912f7e934aa60
Djam18 <adamdjamal18@gmail.com>
2024-04-13 12:00:00 +0100

    feat(integration): calculateurs de taxes

---
+++ app/Http/Controllers/Backend/TaxController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TaxController extends Controller
+{
+    // Contrôleur pour TaxController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ app/Services/TaxCalculationService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe TaxCalculationService
+class TaxCalculationService
+{
+    // Implémentation à venir
+}
