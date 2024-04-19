000d1b1e7e6b1098116dba00368641032ded5e19
Djam18 <adamdjamal18@gmail.com>
2024-04-19 12:00:00 +0100

    feat(integration): traduction automatique

---
+++ app/Http/Controllers/Backend/TranslationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TranslationController extends Controller
+{
+    // Contrôleur pour TranslationController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ app/Services/TranslationService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe TranslationService
+class TranslationService
+{
+    // Implémentation à venir
+}
