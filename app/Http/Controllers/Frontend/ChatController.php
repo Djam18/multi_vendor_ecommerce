a6ae9c82e22570b407b9782b698c9d067657bc04
Djam18 <adamdjamal18@gmail.com>
2024-04-22 12:00:00 +0100

    feat(integration): chat en direct

---
+++ app/Http/Controllers/Frontend/ChatController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    // Contrôleur pour ChatController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/frontend/chat.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend;
+
+// Classe chat.blade
+class chat.blade
+{
+    // Implémentation à venir
+}
