5d7852ea6e3951e164bafe8c2998b9c75bd2e380
Djam18 <adamdjamal18@gmail.com>
2024-04-25 12:00:00 +0100

    feat(integration): chatbot

---
+++ app/Http/Controllers/Frontend/ChatbotController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ChatbotController extends Controller
+{
+    // Contrôleur pour ChatbotController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ app/Services/ChatbotService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe ChatbotService
+class ChatbotService
+{
+    // Implémentation à venir
+}
