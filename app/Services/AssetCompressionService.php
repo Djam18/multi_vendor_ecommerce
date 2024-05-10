1ebf9ba27af3b962318f711cacdc0e6a85711928
Djam18 <adamdjamal18@gmail.com>
2024-05-10 12:00:00 +0100

    perf: compression des assets

---
+++ app/Services/AssetCompressionService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe AssetCompressionService
+class AssetCompressionService
+{
+    // Implémentation à venir
+}
---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+/**
+ * webpack.mix JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour webpack.mix
