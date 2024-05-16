0047f836e74ea25ba8f321684ed2cedd16f06466
Djam18 <adamdjamal18@gmail.com>
2024-05-16 12:00:00 +0100

    perf: lazy loading

---
+++ resources/js/lazy-load.js
@@ -0,0 +1,7 @@
+/**
+ * lazy-load JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour lazy-load
---
+++ resources/views/components/lazy-component.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\components;
+
+// Classe lazy-component.blade
+class lazy-component.blade
+{
+    // Implémentation à venir
+}
