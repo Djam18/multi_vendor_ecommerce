5e28956b8214e332b7d40a4c2ea0e95b265f808b
Djam18 <adamdjamal18@gmail.com>
2024-06-03 12:00:00 +0100

    fix: corrections de bugs fonctionnels

---
+++ app/Http/Controllers/BugFixController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BugFixController extends Controller
+{
+    // Contrôleur pour BugFixController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ app/Services/FixService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe FixService
+class FixService
+{
+    // Implémentation à venir
+}
