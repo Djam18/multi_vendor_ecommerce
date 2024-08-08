4f121d2755a0b17b9f80a9c803ec931ff0d57b8a
Djam18 <adamdjamal18@gmail.com>
2024-08-08 12:00:00 +0100

    feat(advanced): amélioration de l'expérience d'achat

---
+++ resources/js/shopping-experience.js
@@ -0,0 +1,7 @@
+/**
+ * shopping-experience JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour shopping-experience
---
+++ resources/views/frontend/components/experience.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\components;
+
+// Classe experience.blade
+class experience.blade
+{
+    // Implémentation à venir
+}
