d1dacc5fac31f5b2247269a51ae8f706a8c4e058
Djam18 <adamdjamal18@gmail.com>
2024-09-01 12:00:00 +0100

    feat(exclusive): programme de fidélité avancé

---
+++ app/Http/Controllers/Backend/LoyaltyProgramController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoyaltyProgramController extends Controller
+{
+    // Contrôleur pour LoyaltyProgramController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
