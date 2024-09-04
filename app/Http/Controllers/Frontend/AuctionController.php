ae190b17ea141d7cf85a1aeaf4f01dc5d7da1808
Djam18 <adamdjamal18@gmail.com>
2024-09-04 12:00:00 +0100

    feat(exclusive): système d'enchères

---
+++ app/Http/Controllers/Frontend/AuctionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuctionController extends Controller
+{
+    // Contrôleur pour AuctionController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
