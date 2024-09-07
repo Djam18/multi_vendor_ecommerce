a7c3cfb42ca7bef643d384b4c403866cd239f391
Djam18 <adamdjamal18@gmail.com>
2024-09-07 12:00:00 +0100

    feat(exclusive): marketplace d'articles numériques

---
+++ app/Http/Controllers/Frontend/DigitalProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DigitalProductController extends Controller
+{
+    // Contrôleur pour DigitalProductController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
