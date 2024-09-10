1b0ba7488cb873601a7e0124487736d89ca9a272
Djam18 <adamdjamal18@gmail.com>
2024-09-10 12:00:00 +0100

    feat(exclusive): système de réservation de produits

---
+++ app/Http/Controllers/Frontend/ReservationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReservationController extends Controller
+{
+    // Contrôleur pour ReservationController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
