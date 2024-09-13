6889a51023ca902b79f4df5b8e890c76854f904b
Djam18 <adamdjamal18@gmail.com>
2024-09-13 12:00:00 +0100

    feat(exclusive): système de précommande

---
+++ app/Http/Controllers/Frontend/PreorderController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PreorderController extends Controller
+{
+    // Contrôleur pour PreorderController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
