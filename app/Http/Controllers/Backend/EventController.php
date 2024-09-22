8a5ba15eac017773288556b904f67c2d76b16f22
Djam18 <adamdjamal18@gmail.com>
2024-09-22 12:00:00 +0100

    feat(exclusive): système de gestion d'événements

---
+++ app/Http/Controllers/Backend/EventController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    // Contrôleur pour EventController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
