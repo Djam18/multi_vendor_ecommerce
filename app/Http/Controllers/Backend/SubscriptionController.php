fa499766103960dfdcbf79b50cbdc84cc5561bac
Djam18 <adamdjamal18@gmail.com>
2024-10-01 12:00:00 +0100

    feat(exclusive): système d'abonnement premium

---
+++ app/Http/Controllers/Backend/SubscriptionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubscriptionController extends Controller
+{
+    // Contrôleur pour SubscriptionController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
