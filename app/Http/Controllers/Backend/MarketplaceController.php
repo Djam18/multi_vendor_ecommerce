ce7480dfed9c8ddde70312474519efdc79a6c462
Djam18 <adamdjamal18@gmail.com>
2024-10-07 12:00:00 +0100

    feat(exclusive): système de marketplaces multiples

---
+++ app/Http/Controllers/Backend/MarketplaceController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MarketplaceController extends Controller
+{
+    // Contrôleur pour MarketplaceController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
