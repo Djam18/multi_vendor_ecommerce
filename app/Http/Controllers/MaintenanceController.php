30539665b7f29d96bc4d755aafc143d2e290a59e
Djam18 <adamdjamal18@gmail.com>
2024-11-24 12:00:00 +0100

    feat(launch): page de maintenance

---
+++ app/Http/Controllers/MaintenanceController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MaintenanceController extends Controller
+{
+    // Contrôleur pour MaintenanceController
+    
+    public function index()
+    {
+        // Logique d'index
+    }
+}
---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views;
+
+// Classe maintenance.blade
+class maintenance.blade
+{
+    // Implémentation à venir
+}
