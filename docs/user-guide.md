d13e8007fd1207f126006c6905d7c39858fc7101
Djam18 <adamdjamal18@gmail.com>
2024-12-30 12:00:00 +0100

    docs: finalisation du guide d'utilisation

---
+++ docs/user-guide.md
@@ -0,0 +1,3 @@
+# User-Guide
+
+Documentation pour user-guide.
---
+++ resources/views/frontend/help/user-guide.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\help;
+
+// Classe user-guide.blade
+class user-guide.blade
+{
+    // Implémentation à venir
+}
