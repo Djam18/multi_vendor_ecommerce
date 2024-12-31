10ab1f7221e42b31dd265b4f661d03054123e128
Djam18 <adamdjamal18@gmail.com>
2024-12-31 12:00:00 +0100

    feat: système de rollback automatique

---
+++ app/Console/Commands/RollbackCommand.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Console\Commands;
+
+// Classe RollbackCommand
+class RollbackCommand
+{
+    // Implémentation à venir
+}
---
+++ app/Services/RollbackService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+// Classe RollbackService
+class RollbackService
+{
+    // Implémentation à venir
+}
