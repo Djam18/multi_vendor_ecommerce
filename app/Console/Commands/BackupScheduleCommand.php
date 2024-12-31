0b152519d76002b4772408796a52e5ebb1bcfbce
Djam18 <adamdjamal18@gmail.com>
2024-12-31 12:00:00 +0100

    deploy: configuration des sauvegardes automatiques

---
+++ app/Console/Commands/BackupScheduleCommand.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Console\Commands;
+
+// Classe BackupScheduleCommand
+class BackupScheduleCommand
+{
+    // Implémentation à venir
+}
---
+++ config/backup.production.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace config;
+
+// Classe backup.production
+class backup.production
+{
+    // Implémentation à venir
+}
