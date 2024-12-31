58c15d384b32a1dfe3123b045df823e0fe6df780
Djam18 <adamdjamal18@gmail.com>
2024-12-31 12:00:00 +0100

    chore: derniers ajustements du tableau de bord admin

---
+++ resources/js/admin-dashboard-final.js
@@ -0,0 +1,7 @@
+/**
+ * admin-dashboard-final JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour admin-dashboard-final
---
+++ resources/views/admin/dashboard/final.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\admin\dashboard;
+
+// Classe final.blade
+class final.blade
+{
+    // Implémentation à venir
+}
