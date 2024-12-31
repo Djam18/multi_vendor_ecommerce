e1377e4ee5bb68f53bedf0502c214ba951d470a2
Djam18 <adamdjamal18@gmail.com>
2024-12-31 12:00:00 +0100

    chore: derniers ajustements du tableau de bord vendeur

---
+++ resources/js/vendor-dashboard-final.js
@@ -0,0 +1,7 @@
+/**
+ * vendor-dashboard-final JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour vendor-dashboard-final
---
+++ resources/views/vendor/dashboard/final.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\vendor\dashboard;
+
+// Classe final.blade
+class final.blade
+{
+    // Implémentation à venir
+}
