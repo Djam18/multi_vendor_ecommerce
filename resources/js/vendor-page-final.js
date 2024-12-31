91996650a286d7fe1b4169ec61cb6b0f5869576e
Djam18 <adamdjamal18@gmail.com>
2024-12-31 12:00:00 +0100

    chore: derniers ajustements des pages vendeurs

---
+++ resources/js/vendor-page-final.js
@@ -0,0 +1,7 @@
+/**
+ * vendor-page-final JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour vendor-page-final
---
+++ resources/views/frontend/vendor/final.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\vendor;
+
+// Classe final.blade
+class final.blade
+{
+    // Implémentation à venir
+}
