5ba3cb8fcfa2cc7f3b99ae097e9ab0b33c1dc5ea
Djam18 <adamdjamal18@gmail.com>
2024-12-31 12:00:00 +0100

    chore: derniers ajustements de la page d'accueil

---
+++ resources/js/home-final.js
@@ -0,0 +1,7 @@
+/**
+ * home-final JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour home-final
---
+++ resources/views/frontend/home-final.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend;
+
+// Classe home-final.blade
+class home-final.blade
+{
+    // Implémentation à venir
+}
