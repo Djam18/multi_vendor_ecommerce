1751418836cafa1283da54afa18a6a90a932053d
Djam18 <adamdjamal18@gmail.com>
2024-12-31 12:00:00 +0100

    chore: derniers ajustements du checkout

---
+++ resources/js/checkout-final.js
@@ -0,0 +1,7 @@
+/**
+ * checkout-final JavaScript
+ * 
+ * @author Djam18 <adamdjamal18@gmail.com>
+ */
+
+// Code JavaScript pour checkout-final
---
+++ resources/views/frontend/checkout/final.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace resources\views\frontend\checkout;
+
+// Classe final.blade
+class final.blade
+{
+    // Implémentation à venir
+}
